eb9e96e95595189dc8d288a2391f951776544f7b
Gavril Marian <B00165945@mytudublin.ie>
2026-03-25 01:10:15 +0000

    Step 6- Creating a Simple UI to Create and Assign Roles and Permissions for Spatie

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,14 @@ Route::resource('bookings', App\Http\Controllers\BookingController::class);
 
 Route::get('/loggedInMember', 'App\Http\Controllers\memberController@getLoggedInMemberDetails');
 
+
+
+Route::resource('users', App\Http\Controllers\usersController::class);
+
+
+
+Route::resource('roles', App\Http\Controllers\rolesController::class);
+
+Route::get('/users/assignroles/{id}', 'App\Http\Controllers\usersController@assignRoles')->name('users.assignroles');
+
+Route::patch('/users/updateroles/{id}', 'App\Http\Controllers\usersController@updateRoles')->name('roles.rolesupdate');
